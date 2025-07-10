f26fc2a8af5313382e44a142b16443b4c2f4322f
vaudauxo-uga <olivia.vaudaux@univ-grenoble-alpes.fr>
2025-07-10 10:30:38 +0200

    feat(utils): New functions to get repo root and local slf output paths

--- scripts/utils.py
+++ scripts/utils.py
@@ -17,3 +17,20 @@ def extract_subject_id_from_filename(edf_file: Path) -> Optional[str]:
     visit_suffix: str = match.group(2)
 
     return f"{patient_id}_{visit_suffix}" if visit_suffix else patient_id
+
+def get_repo_root() -> Path:
+    """Get the root of the repository."""
+
+    repo_root: Path = Path(__file__).resolve().parent
+    while ".git" not in [p.name for p in repo_root.iterdir()]:
+        repo_root = repo_root.parent
+    return repo_root
+
+def get_local_slf_output() -> Path:
+    """Get the root of the local output directory."""
+
+    repo_root: Path = get_repo_root()
+    outside_repo_dir: Path = repo_root.parent
+    local_slf_output: Path = outside_repo_dir / "slf-output"
+    local_slf_output.mkdir(parents=True, exist_ok=True)
+    return local_slf_output
