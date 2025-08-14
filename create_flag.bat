78bd527b33a6928db8dd98e82f1fda659faf6ba9
vaudauxo-uga <olivia.vaudaux@univ-grenoble-alpes.fr>
2025-08-14 10:31:03 +0200

    chore(create_flag): Allows to create automatically the flag file for snakemake

---
+++ create_flag.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Script to create the abosa_complete.flag file
+
+REM Move to the script's folder
+cd /d "%~dp0"
+
+echo. > abosa_complete.flag
+echo File 'abosa_complete.flag' created successfully!
+pause
