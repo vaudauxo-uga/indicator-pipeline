5b3e7ab690c46ff3fd0436d8ffa29b732e471eb8
vaudauxo-uga <olivia.vaudaux@univ-grenoble-alpes.fr>
2025-08-14 10:33:44 +0200

    chore(create_flag): Change final message

--- create_flag.bat
+++ create_flag.bat
@@ -5,5 +5,5 @@ REM Move to the script's folder
 cd /d "%~dp0"
 
 echo. > abosa_complete.flag
-echo File 'abosa_complete.flag' created successfully!
+echo [OK] File 'abosa_complete.flag' created successfully!
 pause
